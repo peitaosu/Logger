ca41453785fdd8db6050a768b47919aef93767e7
Tony Su <peitaosu@163.com>
2018-12-18 23:40:30 +0800

    init new Logger project

---
+++ Logger/LogEvent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class LogEvent
+    {
+        public LogEvent(DateTimeOffset timestamp, LogEventLevel level, Exception exception)
+        {
+            Timestamp = timestamp;
+            Level = level;
+            Exception = exception;
+        }
+
+        public DateTimeOffset Timestamp { get; }
+        public LogEventLevel Level { get; }
+        public Exception Exception { get; }
+
+        public string RenderMessage(IFormatProvider formatProvider = null)
+        {
+            var writer = new StringWriter(formatProvider);
+            return writer.ToString();
+        }
+    }
+
+    public enum LogEventLevel
+    {
+        Verbose,
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Fatal
+    }
+
+}
