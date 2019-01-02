f952239ebf09cf19a38b70eab642df6fdd5c4d7b
Tony Su <peitaosu@163.com>
2019-01-03 00:47:25 +0800

    add sample project

---
+++ Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Logger.Instance.LoadConfig();
+            Logger.Logger.Instance.FileLog.LogPath = "New.log";
+            Logger.Logger.Instance.Information("Information");
+            Logger.Logger.Instance.Debug("Debug");
+            Logger.Logger.Instance.Error("Error");
+            Console.Read();
+        }
+    }
+}
