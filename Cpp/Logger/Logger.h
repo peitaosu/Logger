448ec1cc0f35eedf3bdb542426e4fbdab5ef97da
Tony Su <peitaosu@163.com>
2019-02-04 23:05:36 +0800

    add Logger.h

---
+++ Cpp/Logger/Logger.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "LogEvent.h"
+
+namespace Logging {
+    class Logger;
+}
+
+class Logger
+{
+public:
+    Logger();
+    ~Logger();
+    static Logger* Instance() {
+        if (!_logger) _logger = new Logger();
+        return _logger;
+    }
+    void LoadConfig(std::string config);
+    bool IsEnabled(LogEventLevel level);
+    void Write(LogEvent logEvent);
+    void Write(LogEventLevel level, std::string message, LogEventException exception);
+    void Verbose(std::string message, LogEventException exception);
+    void Debug(std::string message, LogEventException exception);
+    void Information(std::string message, LogEventException exception);
+    void Warning(std::string message, LogEventException exception);
+    void Error(std::string message, LogEventException exception);
+    void Fatal(std::string message, LogEventException exception);
+
+private:
+    static Logger* _logger;
+    std::string _defaultConfig = "LogConfig.xml";
+};
+
