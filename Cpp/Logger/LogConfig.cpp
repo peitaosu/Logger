c1a92e030540e074b0963f5055a5b378644273c7
Tony Su <peitaosu@163.com>
2019-02-17 22:34:56 +0800

    define config nodes

--- Cpp/Logger/LogConfig.cpp
+++ Cpp/Logger/LogConfig.cpp
@@ -1,7 +1,18 @@
 #include "LogConfig.h"
+#include "tinyxml2.h"
 
 LogConfig::LogConfig(){
+    tinyxml2::XMLDocument doc;
+    if (doc.LoadFile(this->config.c_str()) != 0)
+    {
+        std::cout << "load xml file failed" << std::endl;
+        return;
+    }
+}
 
+LogConfig::LogConfig(const char* config) {
+    this->config = config;
+    LogConfig();
 }
 
 LogConfig::~LogConfig(){
--- Cpp/Logger/LogConfig.h
+++ Cpp/Logger/LogConfig.h
@@ -1,11 +1,70 @@
 
+#include <iostream>
+#include <list>
+
 namespace Logging {
     class LogConfig;
+    struct LogAppender;
+    struct AppenderFile;
+    struct AppenderPattern;
+    struct AppenderColor;
+    struct LogRoot;
+    struct RootMinLevel;
+    struct RootAppenderRef;
 }
 
 class LogConfig {
 	public:
 		LogConfig();
+        LogConfig(const char* config);
 		~LogConfig();
+    private:
+        std::string config = "LogConfig.xml";
 };
+
+struct AppenderFile
+{
+    std::string Path;
+    std::string AppendTo;
+};
+
+struct AppenderPattern
+{
+    std::string Value;
+};
+
+struct AppenderColor
+{
+    std::string Level;
+    std::string ForeColor;
+    std::string BackColor;
+};
+
+struct LogAppender
+{
+    std::string Name;
+    std::string Type;
+    AppenderFile File;
+    AppenderPattern Pattern;
+    std::list<AppenderColor> Colors;
+};
+
+struct RootMinLevel
+{
+    std::string Value;
+};
+
+struct RootAppenderRef
+{
+    std::string Ref;
+};
+
+struct LogRoot
+{
+    RootMinLevel MinLevel;
+    std::list<RootAppenderRef> RootAppenderRefs;
+};
+
+
+
 
