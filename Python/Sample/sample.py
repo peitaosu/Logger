0a1b69ffab9aaf3004c0dd7b7ce976dad5b5b543
Tony Su <peitaosu@163.com>
2019-04-08 21:14:15 +0800

    update sample and README.md

--- Python/Sample/sample.py
+++ Python/Sample/sample.py
@@ -2,6 +2,9 @@ from Logger import Logger
 
 logger = Logger()
 logger.LoadConfig("LogConfig.xml")
+logger.SetLogPath("New.log")
+logger.SetLogAppendTo(True)
+logger.EnableAppender("ColoredConsoleAppender", True)
 logger.Information("Information")
 logger.Debug("Debug")
 logger.Error("Error")
