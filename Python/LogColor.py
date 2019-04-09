b29c820ecfb239d1cc3071c656444b666423b070
Tony Su <peitaosu@163.com>
2019-04-09 21:58:20 +0800

    update color of colored console output

--- Python/LogColor.py
+++ Python/LogColor.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class LogColor(Enum):
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
