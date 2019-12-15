8b818315cf0ca704953ccd88d46f65f6f36c5bc8
Tony Su <peitaosu@163.com>
2019-12-15 19:48:30 +0800

    update validation function

--- Tools/SchemaValidate.py
+++ Tools/SchemaValidate.py
@@ -1,17 +1,14 @@
 import os, sys
 import lxml
-from lxml import etree
-from io import StringIO
+from lxml import etree, objectify
 
 def XMLValidate(xml_path, xsd_path):
     with open(xml_path) as in_file:
         in_xml = in_file.read()
-    with open(xsd_path) as in_file:
-        in_xsd = in_file.read()
-    xsd_doc = etree.parse(StringIO(in_xsd))
-    xsd = etree.XMLSchema(xsd_doc)
+    schema = etree.XMLSchema(file=xsd_path)
+    parser = objectify.makeparser(schema=schema)
     try:
-        doc = etree.parse(StringIO(in_xml))
+        objectify.fromstring(in_xml, parser)
     except etree.XMLSyntaxError as err:
         print(err.error_log)
 
