068118b6c0263fc183ffbe96f16f59f3f0186731
Tony Su <peitaosu@163.com>
2020-08-28 22:21:41 +0800

    combine changes

--- Simple/Logger.cpp
+++ Simple/Logger.cpp
@@ -1,5 +1,27 @@
 #include "Logger.h"
 
+LogEvent::LogEvent() {
+}
+
+LogEvent::LogEvent(LogEventLevel level, std::string message) {
+    auto time = std::time(nullptr);
+    auto tm = *std::localtime(&time);
+    this->timestamp = tm;
+    this->level = level;
+    this->message = message;
+}
+
+std::tm LogEvent::GetTimestamp() {
+    return this->timestamp;
+}
+LogEventLevel LogEvent::GetLevel() {
+    return this->level;
+}
+std::string LogEvent::GetMessage() {
+    return this->message;
+}
+
+
 Logger::Logger()
 {
 }
--- Simple/Logger.h
+++ Simple/Logger.h
@@ -4,11 +4,38 @@
 #include <ctime>
 #include <iomanip>
 #include <sstream>
-#include "LogEvent.h"
+#include <chrono>
 
 namespace Logging {
     class Logger;
+    class LogEvent;
+    enum LogEventLevel;
 }
+
+enum LogEventLevel
+{
+    VERBO,
+    DEBUG,
+    INFOR,
+    WARNN,
+    ERROR,
+    FATAL
+};
+
+class LogEvent
+{
+public:
+    LogEvent();
+    LogEvent(LogEventLevel level, std::string message);
+    std::tm GetTimestamp();
+    LogEventLevel GetLevel();
+    std::string GetMessage();
+
+private:
+    std::tm timestamp;
+    LogEventLevel level;
+    std::string message;
+};
 
 class Logger
 {
